3b022ca67f8627eb223a20b1bdc49d5a5a6a091d
Abuhurrara <abuhurrara1093@gmail.com>
2025-05-16 23:59:35 +0500

    update cv

--- utils/data/contactsData.js
+++ utils/data/contactsData.js
@@ -1,7 +1,7 @@
 export const contactsData = {
     email: 'abuhurrara1093@gmail.com',
-    phone: '+92 3317191001',
-    address: 'Near PTCL Exchange, Toba tek Singh, Punjab, Pakistan',
+    phone: '+92 331 7191001',
+    address: 'Bahria Town, Lahore, Pakistan',
     github: 'https://github.com/Abuhurrara',
     facebook: 'https://www.facebook.com/profile.php?id=100005583424479',
     linkedIn: 'https://www.linkedin.com/in/abu-hurrara-ali-094677216/',
--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -5,7 +5,7 @@ export const personalData = {
   description: "My name is ABU HURRARA. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on JavaScript and I love to do most of the things using JavaScript. I am available for any kind of job opportunity that suits my skills and interests.",
   email: 'abuhurrara1093@gmail.com',
   phone: '+92 3317191001',
-  address: 'Near PTCL Exchange, Toba tek Singh, Punjab, Pakistan',
+  address: 'Bahria Town, Lahore, Pakistan',
   github: 'https://github.com/Abuhurrara',
   facebook: 'https://www.facebook.com/profile.php?id=100005583424479',
   linkedIn: 'https://www.linkedin.com/in/abu-hurrara-ali-094677216/',
@@ -13,5 +13,5 @@ export const personalData = {
   stackOverflow: 'https://stackoverflow.com/users/16840768/abuhurrara',
   leetcode: "https://leetcode.com/abuhurrara/",
   devUsername: "abuhurrara",
-  resume: "https://drive.google.com/file/d/1eyutpKFFhJ9X-qpQGKhUNnVRkB5Wer00/view?usp=sharing"
+  resume: "https://drive.google.com/file/d/1NoSNYfVVmMkpQ-6Mu_Dx-aWrDcUjy1Dj/view?usp=sharing"
 }
